ed033ec45061af1b7632ca8ee01b036eb04f0de7
ujeonn <cherixy1128@naver.com>
2023-04-18 10:56:06 +0900

    프론트엔드 8.1~2 React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Todo from './Todo';
 import './App.css';
-class App extends React.Component { constructor(props){ //매개변수 props 생성자
- super(props); // 매개변수 pros 초기화
- this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
- item :{id:0, title:"Hello World 1 ", done:true}, }; } render(){ return ( // 매개변수 item 에 변수명/값을 전달
- <div className="App">
- <Todo item={this.state.item}/>
- </div>
- ); }
+class App extends React.Component {
+  constructor(props){ //매개변수 props 생성자
+    super(props); // 매개변수 pros 초기화
+    this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      items :[
+        {id:0, title:"Todo 1 ", done:false},
+        {id:1, title:"Todo 2 ", done:false},
+       ], 
+    }; 
+  } 
+  render(){ 
+    var todoItems =this.state.items.map((item,idx)=>(
+      <Todo item={item} key={item.id}/>
+    ));
+
+    return <div className="App">{todoItems}</div>;
+  }
 }
 export default App;
